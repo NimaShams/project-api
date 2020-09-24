2999a4d8cf43fea1d7a6b51bbbb11a27a4c36ff9
NiMa <n.feyzshams@gmail.com>
2020-09-24 14:09:41 +0330

    add rest api project to my git for resume

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import axios from 'axios'
+
+import Post from '../../components/Post/Post'
+import FullPost from '../../components/FullPost/FullPost'
+import NewPost from '../../components/NewPost/NewPost'
+
+import './Blog.css'
+
+class Blog extends React.Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false,
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        const posts = response.data.slice(0, 4)
+        const updatedPosts = posts.map((item) => {
+          return {
+            ...item,
+            author: 'Masood',
+          }
+        })
+        this.setState({ posts: updatedPosts })
+      })
+      .catch((err) => {
+        this.setState({ error: true })
+      })
+  }
+
+  selectPostHabdler = (id) => {
+    this.setState({ selectedPostId: id })
+  }
+
+  render() {
+    let posts = <p style={{ textAlign: 'center' }}>Fetching data failed!</p>
+    if (!this.state.error) {
+      posts = this.state.posts.map((item) => {
+        return (
+          <Post
+            key={item.id}
+            title={item.title}
+            author={item.author}
+            click={() => this.selectPostHabdler(item.id)}
+          />
+        )
+      })
+    }
+
+    return (
+      <div>
+        <section className="posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    )
+  }
+}
+
+export default Blog
